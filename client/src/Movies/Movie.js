8a9eb6f418343b2ca19ff528c693a12b8638b20b
cammac <camm@rocktechcomputer.com>
2019-04-08 13:46:32 -0700

    completed movies functionality

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -7,11 +7,13 @@ export default class Movie extends Component {
     this.state = {
       movie: null
     };
-  }
+  };
+
 
   componentDidMount() {
+
     // change this line to grab the id passed on the URL
-    const id = 1;
+    const id = this.props.match.params.id;
     this.fetchMovie(id);
   }
 
